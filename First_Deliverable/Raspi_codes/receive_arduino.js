5529a428f5ef2f7d93e8a21735b3996ccbac658d
Sneha Pradhan <snehha@bu.edu>
2016-11-14 20:56:21 -0500

    changes

--- First_Deliverable/Raspi_codes/receive_arduino.js
+++ First_Deliverable/Raspi_codes/receive_arduino.js
@@ -3,10 +3,37 @@ noble.startScanning();
 
 noble.startScanning([], true); // any service UUID, allow duplicates
 
-var serviceUUIDs = ["<service UUID 1>", ...]; // default: [] => all
+var IMU_SERVICE_UUID = '19B10000-E8F2-537E-4F6C-D104768A1214';
 var allowDuplicates = <false|true>; // default: false
 
-noble.startScanning(serviceUUIDs, allowDuplicates[, callback(error)]); // particular UUID's
+noble.on('stateChange', function(state) {
+  if(state === 'poweredOn') {
+    console.log('Start BLE scan...')
+    noble.startScanning([IMU_SERVICE_UUID], false);
+  }
+  else {
+    console.log('Cannot scan... state is not poweredOn')
+    noble.stopScanning();
+  }
+});
+
+// Discover the peripheral's IMU service and corresponding characteristics
+// Then, emit each data point on the socket stream
+// noble.on('discover', function(peripheral) {
+//   peripheral.connect(function(error) {
+//     console.log('Connected to peripheral: ' + peripheral.uuid);
+//     peripheral.discoverServices([IMU_SERVICE_UUID], function(error, services) {
+//       var imuService = services[0];
+//       console.log('Discovered IMU service');
+
+//       imuService.discoverCharacteristics([], function(error, characteristics) {
+//         characteristics.forEach(function(characteristic) {
+//           emitSensorData(characteristic);
+//         });
+//       });
+//     });
+//   });
+// });
 
 noble.on(‘discover’, function(peripheral) { 
 
@@ -14,4 +41,4 @@ noble.on(‘discover’, function(peripheral) {
   var rss = peripheral.rssi;
   var localName = advertisement.localName; 
   console.log('found device: ', macAdress, ' ', localName, ' ', rss);   
-}
+};
