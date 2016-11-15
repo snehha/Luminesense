44551ff54760b2f76ba4377122296dc4e3ef32ee
Sneha <snehha@bu.edu>
2016-11-15 02:00:10 +0000

    added

--- First_Deliverable/Raspi_codes/receive_arduino.js
+++ First_Deliverable/Raspi_codes/receive_arduino.js
@@ -36,9 +36,8 @@ noble.on('stateChange', function(state) {
 // });
 
 noble.on('discover', function(peripheral) { 
-
   var macAddress = peripheral.uuid;
   var rss = peripheral.rssi;
   var localName = advertisement.localName; 
   console.log('found device: ', macAdress, ' ', localName, ' ', rss);   
-};
+});
