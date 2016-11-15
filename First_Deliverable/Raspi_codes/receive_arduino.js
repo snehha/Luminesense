ae5d335fd97dc8e767415b0e946f3561b67901d3
Sneha <snehha@bu.edu>
2016-11-15 02:39:30 +0000

    Changes

--- First_Deliverable/Raspi_codes/receive_arduino.js
+++ First_Deliverable/Raspi_codes/receive_arduino.js
@@ -1,50 +1,40 @@
 var noble = require('noble');
-noble.startScanning();
 
-noble.startScanning([], true); // any service UUID, allow duplicates
 
 var IMU_SERVICE_UUID = '19B10000-E8F2-537E-4F6C-D104768A1214';
-noble.on('stateChange', function(state) {
-  if (state === 'poweredOn') {
-    noble.startScanning();
-  } else {
-    noble.stopScanning();
-  }
-});
-noble.on('discover', function(peripheral) { 
-  var macAddress = peripheral.uuid;
-  var rss = peripheral.rssi;
-  var localName = advertisement.localName; 
-  console.log('found device: ', macAdress, ' ', localName, ' ', rss);   
-});
 
-// noble.on('stateChange', function(state) {
-//   if(state === 'poweredOn') {
-//     console.log('Start BLE scan...')
-//     noble.startScanning([IMU_SERVICE_UUID], false);
-//   }
-//   else {
-//     console.log('Cannot scan... state is not poweredOn')
-//     noble.stopScanning();
-//   }
-// });
+noble.on('stateChange', function(state) {
+   if(state === 'poweredOn') {
+     console.log('Start BLE scan...')
+     noble.startScanning([IMU_SERVICE_UUID], false);
+   }
+   else {
+     console.log('Cannot scan... state is not poweredOn')
+     noble.stopScanning();
+   }
+});
 
 // Discover the peripheral's IMU service and corresponding characteristics
 // Then, emit each data point on the socket stream
-// noble.on('discover', function(peripheral) {
-//   peripheral.connect(function(error) {
-//     console.log('Connected to peripheral: ' + peripheral.uuid);
-//     peripheral.discoverServices([IMU_SERVICE_UUID], function(error, services) {
-//       var imuService = services[0];
-//       console.log('Discovered IMU service');
+noble.on('discover', function(peripheral) {
+   console.log('peripheral discovered (' + peripheral.id +
+              ' with address <' + peripheral.address +  ', ' + peripheral.addressType + '>,' +
+              ' connectable ' + peripheral.connectable + ',' +
+              ' RSSI ' + peripheral.rssi + ':');
+    console.log('\thello my local name is:');
+   peripheral.connect(function(error) {
+     console.log('Connected to peripheral: ' + peripheral.uuid);
+     peripheral.discoverServices([IMU_SERVICE_UUID], function(error, services) {
+       var imuService = services[0];
+       console.log('Discovered IMU service');
 
-//       imuService.discoverCharacteristics([], function(error, characteristics) {
-//         characteristics.forEach(function(characteristic) {
-//           emitSensorData(characteristic);
-//         });
-//       });
-//     });
-//   });
-// });
+       imuService.discoverCharacteristics([], function(error, characteristics) {
+         characteristics.forEach(function(characteristic) {
+           emitSensorData(characteristic);
+         });
+       });
+     });
+   });
+});
 
 
