979a8b6fc46f01d1b6bdc7943086d32b5a87dff6
Sneha Pradhan <snehha@bu.edu>
2016-11-14 20:52:03 -0500

    changes

--- First_Deliverable/Raspi_codes/receive_arduino.js
+++ First_Deliverable/Raspi_codes/receive_arduino.js
@@ -1,6 +1,13 @@
 var noble = require('noble');
 noble.startScanning();
 
+noble.startScanning([], true); // any service UUID, allow duplicates
+
+var serviceUUIDs = ["<service UUID 1>", ...]; // default: [] => all
+var allowDuplicates = <false|true>; // default: false
+
+noble.startScanning(serviceUUIDs, allowDuplicates[, callback(error)]); // particular UUID's
+
 noble.on(‘discover’, function(peripheral) { 
 
   var macAddress = peripheral.uuid;
