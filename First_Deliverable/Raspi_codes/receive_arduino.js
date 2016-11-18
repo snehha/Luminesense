f5776f2ca4d41c15c3794643a8873e79898e2b63
Sneha Pradhan <snehha@bu.edu>
2016-11-18 01:00:32 -0500

    test

--- First_Deliverable/Raspi_codes/receive_arduino.js
+++ First_Deliverable/Raspi_codes/receive_arduino.js
@@ -1,7 +1,7 @@
 var noble = require('noble');
 var Particle = require('particle-api-js');
 var particle = new Particle();  //Particle part 
-
+var on_off;
 // MODIFY THIS WITH THE APPROPRIATE URL
 var socket = require('socket.io-client')('WEB-SERVER-DOMAIN-HERE:8080');
 var peripheralIdOrAddress = process.argv[2].toLowerCase();
@@ -80,7 +80,7 @@ function onCharacteristicDiscovered(error, characteristics) {
 
 function onIMUCharacteristicsRead(data, isNotification) {
   console.log('imuCharacteristic read response value: ', data.readInt8(0));
-  var on_off = data.readInt8(0);
+  on_off = data.readInt8(0);
 }
 
 
