9de5c692bd2397935e8ec120c0dd7242949b9ec0
Caroline <ctjones@bu.edu>
2017-03-25 16:43:43 -0400

    integration with database

--- receive_arduino_test.js
+++ receive_arduino_test.js
@@ -2,6 +2,10 @@ var noble = require('noble');
 var Particle = require('particle-api-js');
 var particle = new Particle();  //Particle part 
 var on_off;
+var pg = require('pg');
+var types = require('pg').types;
+var WATTAGE = 12;
+var numOn = 0;
 // MODIFY THIS WITH THE APPROPRIATE URL
 var socket = require('socket.io-client')('WEB-SERVER-DOMAIN-HERE:8080');
 var peripheralIdOrAddress = process.argv[2].toLowerCase();
@@ -10,6 +14,23 @@ var IMU_SERVICE_UUID = "0bdb190aabad11e680f576304dec7eb7";
 var LIGHT_ID_UUID = "0bdb1c0cabad11e680f576304dec7eb7";
 var IMU_READINGS_UUID = "0bdb1d92abad11e680f576304dec7eb7";
 var SESSION_ID_UUID = "19b10001e8f2537e4f6cd104768a1214";
+
+var client = new pg.Client({
+	user: "jryhvlrzsvchoc",
+	password: "0fece6e968bfa67a69e4ed643f60fb9aeb8d7d29d1eada2a493ce5faeef8787b",
+	database: "d9obmj9ncrr8al",
+	port: 5432,
+	host: "ec2-23-21-111-81.compute-1.amazonaws.com",
+	ssl: true
+});
+client.connect();
+
+//calls checkDatabase every hour
+setInterval(checkDatabase, 1000 * 60 * 60);
+
+function databasePost(){
+    client.query('INSERT INTO time (timing, lightson) VALUES ($1, $2);', [new Date(), numOn]);
+}
 
 socket.on('connect', function() {
   console.log('Connected to server');
@@ -143,6 +164,8 @@ function functionPost() {
       argument: '1,u', 
       auth: token
     });
+    numOn = numOn + 1;
+    databasePost();
   }
   else if(on_off == 100){
     var fnPr = particle.callFunction({ 
@@ -151,6 +174,8 @@ function functionPost() {
       argument: '1,d', 
       auth: token
     });
+    numOn = numOn - 1;
+    databasePost();
   } 
   else if(on_off == 98){ // 'r'
     var fnPr = particle.callFunction({ 
@@ -176,7 +201,48 @@ function functionPost() {
   });
 }
 
+//check to see if it has been 24 hours since the last update
+function checkDatabase(){
+	var query = client.query('SELECT day FROM dailystats', [], function(err,result){
+      if (err) throw err;
+      var currDate = new Date(Date.now());
+      //check if there is an entry
+      if (result.rows.length){
+      	var dataDate = new Date(Date.parse(result.rows[result.rows.length - 1].day));
+        var timeDiff = Math.abs(dataDate.getTime() - currDate.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        if (diffDays >= 2){
+          databaseUpdate();
+        }
+      }
 
+      //if there is no entry, add one
+      else{
+      	databaseUpdate();
+      }
+      
+    });
+};
+
+//parse values from time database, upload into dailystats database
+function databaseUpdate(){
+	var kwh = 0;
+	var query = client.query('SELECT timing, lightson FROM time', [], function(err,result){
+		if (err) throw err;
+		var date = new Date(Number(result.rows[0].timing));
+		var parseDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+		var i = 1;
+		for (i = 1; i < result.rows.length; i++){
+			var timediff = Math.abs(Number(result.rows[i].timing) - Number(result.rows[i - 1].timing));
+			bulbs = result.rows[i - 1].lightson;
+			kwh += (WATTAGE * bulbs * (timediff / (1000 * 60 * 60)) / 1000);
+		}
+		//var last = new Date(Number(result.rows[i - 1].timing));
+		//client.query('DELETE FROM time WHERE timing < $1;', [last]);
+		client.query('DELETE FROM time');
+		client.query('INSERT INTO dailystats (kw, day) VALUES ($1, $2);', [kwh, new Date()]);
+	});
+}
 
 //Socket stuff-- used later 
 /*
