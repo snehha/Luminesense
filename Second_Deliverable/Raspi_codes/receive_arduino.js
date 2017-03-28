922f43ded2c63ab81b75f658e7ef84338281cc25
Sneha <snehha@bu.edu>
2017-03-28 21:16:05 +0000

    Clean up

--- Second_Deliverable/Raspi_codes/receive_arduino.js
+++ Second_Deliverable/Raspi_codes/receive_arduino.js
@@ -1,15 +1,38 @@
 var noble = require('noble');
 var Particle = require('particle-api-js');
+var pg = require('pg');
+var types = require('pg').types;
 var particle = new Particle();  //Particle part 
 var on_off;
+var WATTAGE = 12;
+var numOn = 0;
 // MODIFY THIS WITH THE APPROPRIATE URL
 var socket = require('socket.io-client')('WEB-SERVER-DOMAIN-HERE:8080');
 var peripheralIdOrAddress = process.argv[2].toLowerCase();
 // These should correspond to the peripheral's service and characteristic UUIDs
 var IMU_SERVICE_UUID = "0bdb190aabad11e680f576304dec7eb7";
-
 var LIGHT_ID_UUID = "0bdb1c0cabad11e680f576304dec7eb7";
-var IMU_READINGS_UUID = "0bdb1d92abad11e680f576304dec7eb7"
+var IMU_READINGS_UUID = "0bdb1d92abad11e680f576304dec7eb7";
+var SESSION_ID_UUID = "19b10001e8f2537e4f6cd104768a1214";
+var SELECTION_ID_UUID = "89afbbdce26a4880b1ea055cf7aa644d";
+
+var client = new pg.Client({
+	user: "jryhvlrzsvchoc",
+	password: "0fece6e968bfa67a69e4ed643f60fb9aeb8d7d29d1eada2a493ce5faeef8787b",
+	database: "d9obmj9ncrr8al",
+	port: 5432,
+	host: "ec2-23-21-111-81.compute-1.amazonaws.com",
+	ssl: true
+});
+client.connect();
+
+//calls checkDatabase every hour
+setInterval(checkDatabase, 1000 * 60 * 60);
+
+function databasePost(){
+    client.query('INSERT INTO time (timing, lightson) VALUES ($1, $2);', [new Date(), numOn]);
+}
+
 socket.on('connect', function() {
   console.log('Connected to server');
   socket.emit('hello');
@@ -47,7 +70,6 @@ noble.on('discover', function(peripheral) {
 
 function onServicesDiscovered(error, services) {
   console.log('services discovered');
-
   services.forEach(function(service) {
     console.log(service.uuid);
     if (service.uuid == IMU_SERVICE_UUID) {
@@ -58,33 +80,52 @@ function onServicesDiscovered(error, services) {
   });
 }
 
-
-function onCharacteristicDiscovered(error, characteristics) {
+function onCharacteristicDiscovered(error, characteristics) 
+{
   console.log('characteristics discovered');
-
-  characteristics.forEach(function(characteristic) {
-    if (characteristic.uuid == IMU_READINGS_UUID) {
+  characteristics.forEach(function(characteristic) 
+  {
+    if (characteristic.uuid == IMU_READINGS_UUID) 
+    {
       characteristic.on('data', onIMUCharacteristicsRead);
-      characteristic.notify(true, function(error) {
+      characteristic.notify(true, function(error) 
+      {
         console.log('imuCharacteristic notification on');
       });
-    } else if (characteristic.uuid == LIGHT_ID_UUID) {
+    } 
+    else if (characteristic.uuid == LIGHT_ID_UUID) 
+    {
       characteristic.on('data', onLightCharacteristicRead);
 
-      characteristic.notify(true, function(error) {
+      characteristic.notify(true, function(error) 
+      {
         console.log('idCharacteristic notification on');
       });
-    }
-  });
-}
+    } 
+    else if (characteristic.uuid == SESSION_ID_UUID) 
+    {
+      characteristic.on('data', onSessionCharacteristicRead);
+      characteristic.notify(true, function(error) 
+      {
+        console.log('sessionidCharacteristic notification on');
+        });
+      }
+    else if (characteristic.uuid == SELECTION_ID_UUID) 
+    {
+      characteristic.on('data', onSelectionCharacteristicRead);
+      characteristic.notify(true, function(error) 
+      {
+        console.log('selectionidCharacteristic notification on');
+        });
+      }
+    });
+ }
 
 function onIMUCharacteristicsRead(data, isNotification) {
   console.log('imuCharacteristic read response value: ', data.readInt8(0));
-  on_off = data.readInt8(0);
-  functionPost();
+  //on_off = data.readInt8(0);
+  //functionPost();
 }
-
-
 
 function onLightCharacteristicRead(data, isNotification) {
   if (isNotification) {
@@ -93,6 +134,28 @@ function onLightCharacteristicRead(data, isNotification) {
     // functionPost();
   } else {
     console.log('idCharacteristic read response value: ', data.readInt8(0));
+    //var light_id = data.readInt8(0);
+  }
+}
+
+function onSessionCharacteristicRead(data, isNotification) {
+  if (isNotification) {
+    console.log('sessionidCharacteristic notification value: ', data.readInt8(0));
+    //var light_id = data.readInt8(0);
+    // functionPost();
+  } else {
+    console.log('sessionidCharacteristic read response value: ', data.readInt8(0));
+    //var light_id = data.readInt8(0);
+  }
+}
+
+function onSelectionCharacteristicRead(data, isNotification) {
+  if (isNotification) {
+    console.log('onSelectionCharacteristic notification value: ', data.readInt32(0));
+    //var light_id = data.readInt8(0);
+    // functionPost();
+  } else {
+    console.log('onSelectionCharacteristic read response value: ', data.readInt32(0));
     //var light_id = data.readInt8(0);
   }
 }
@@ -115,6 +178,8 @@ function functionPost() {
       argument: '1,u', 
       auth: token
     });
+    numOn = numOn + 1;
+    databasePost();
   }
   else if(on_off == 100){
     var fnPr = particle.callFunction({ 
@@ -123,6 +188,8 @@ function functionPost() {
       argument: '1,d', 
       auth: token
     });
+    numOn = numOn - 1;
+    databasePost();
   } 
   else if(on_off == 98){ // 'r'
     var fnPr = particle.callFunction({ 
@@ -148,7 +215,48 @@ function functionPost() {
   });
 }
 
+//check to see if it has been 24 hours since the last update
+function checkDatabase(){
+	var query = client.query('SELECT day FROM dailystats', [], function(err,result){
+      if (err) throw err;
+      var currDate = new Date(Date.now());
+      //check if there is an entry
+      if (result.rows.length){
+      	var dataDate = new Date(Date.parse(result.rows[result.rows.length - 1].day));
+        var timeDiff = Math.abs(dataDate.getTime() - currDate.getTime());
+        var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
+        if (diffDays >= 2){
+          databaseUpdate();
+        }
+      }
 
+      //if there is no entry, add one
+      else{
+      	databaseUpdate();
+      }
+      
+    });
+};
+
+//parse values from time database, upload into dailystats database
+function databaseUpdate(){
+	var kwh = 0;
+	var query = client.query('SELECT timing, lightson FROM time', [], function(err,result){
+		if (err) throw err;
+		var date = new Date(Number(result.rows[0].timing));
+		var parseDate = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate();
+		var i = 1;
+		for (i = 1; i < result.rows.length; i++){
+			var timediff = Math.abs(Number(result.rows[i].timing) - Number(result.rows[i - 1].timing));
+			bulbs = result.rows[i - 1].lightson;
+			kwh += (WATTAGE * bulbs * (timediff / (1000 * 60 * 60)) / 1000);
+		}
+		//var last = new Date(Number(result.rows[i - 1].timing));
+		//client.query('DELETE FROM time WHERE timing < $1;', [last]);
+		client.query('DELETE FROM time');
+		client.query('INSERT INTO dailystats (kw, day) VALUES ($1, $2);', [kwh, new Date()]);
+	});
+}
 
 //Socket stuff-- used later 
 /*
