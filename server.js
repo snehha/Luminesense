da226dca494bae5d118142ce9ecd553bedabd05b
Caroline <ctjones@bu.edu>
2017-04-15 15:44:01 -0400

    on off button

--- server.js
+++ server.js
@@ -74,7 +74,7 @@ app.post('/gestures.html', function(req, res){
 
 app.post('/lightchange', function(req, res){
 	console.log("we're in");
-	console.log(req.body.myonoffswitch);
+	console.log(req.body.onoffswitch);
 });
 
 app.listen(port, function() {
